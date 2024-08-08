35010895cc08011d6cc760ea90ffdd7e4ef1adba
Compeador <compeador@universetech.cc>
2024-08-08 22:38:07 +0800

    fix: limit param

--- app/controllers/api/v1/bus_routes_controller.rb
+++ app/controllers/api/v1/bus_routes_controller.rb
@@ -15,9 +15,9 @@ module Api::V1
     # shows all routes and stops info of given city
     def show
       page = params[:page].to_i
-      cache_key = "bus_routes_show_#{params[:city]}_#{page}"
+      cache_key = "bus_routes_show_#{params[:city]}@#{page}"
       @bus_route = Rails.cache.fetch(cache_key, expires_in: 1.hour) do
-        TdxApi.new.get_city_routes params[:city]
+        TdxApi.new.get_city_routes params[:city], page
       end
       render json: { error: 'Invalid parameter' }, status: :bad_request if @bus_route.nil?
       render json: @bus_route
@@ -26,9 +26,10 @@ module Api::V1
     # GET /bus_routes/:city/routes/:route_name
     # get route status
     def route_show
-      cache_key = "bus_routes_show_#{params[:city]}_#{params[:route_name]}"
+      page = params[:page].to_i
+      cache_key = "bus_routes_show_#{params[:city]}_#{params[:route_name]}@#{page}"
       @realtime_route = Rails.cache.fetch(cache_key, expires_in: 1.minute) do
-        TdxApi.new.get_live_route params[:city], params[:route_name]
+        TdxApi.new.get_live_route params[:city], params[:route_name], page
       end
       render json: { error: 'Invalid parameter' }, status: :bad_request if @realtime_route.nil?
       render json: @realtime_route
