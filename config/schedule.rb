0ea4dd222c8464d76d45a22064e63de3866f8811
Compeador <compeador@universetech.cc>
2024-08-09 16:26:00 +0800

    fix: cron worker

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,7 @@
-env :PATH, ENV['PATH']
-set :output, "log/cron_output.log"
+ENV.each { |k, v| env(k, v) }
+set :output, {:standard => "#{path}/log/cron.log", :error => "#{path}/log/cron_error.log"}
 set :environment, ENV['RAILS_ENV']
 
-every 1.minute do
-  runner "Api::V1::WatchedRoutesController.update_bus_live"
-end
-
+every 1.minutes do
+  command "cd #{path} && bin/rails runner 'Api::V1::WatchedRoutesController.update_bus_live'"
+end 
